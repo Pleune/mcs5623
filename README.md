63fc553c5ad9e7bdb2e340b326601362cf658b4e
Mitchell Pleune <mitchpleune@gmail.com>
2024-09-04 07:35:47 -0400

    init with jupyterlab and scikit-learn

---
+++ README.md
@@ -0,0 +1,7 @@
+Run a jupyterlab server, including deps defined in the pyporject.toml with
+
+```bash
+nix run .
+# or
+nix run git+https://git.pleunetowne.com/mitchpleune/mcs5623.git
+```
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Mitchell Pleunes Nix flake for mcs5623 workspace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs =
+    inputs@{ flake-parts, poetry2nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, config, ... }:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
+        in
+        {
+          apps.default = {
+            type = "app";
+            program = pkgs.writeShellApplication {
+              name = "poetry-defined-jupyter-lab";
+              runtimeInputs = [
+                (mkPoetryEnv {
+                  projectDir = ./.;
+                  groups = [ "jupyter" ];
+                })
+              ];
+              text = "jupyter lab";
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [ (mkPoetryEnv { projectDir = ./.; }) ];
+          };
+        };
+    };
+}
